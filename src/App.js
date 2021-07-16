7af6ce2393ddf6e4dd9e763b1d56eb266ba86095
jayson3110 <jayson7498@gmail.com>
2021-07-17 00:48:24 +0700

    Card , processing Chart

--- src/App.js
+++ src/App.js
@@ -3,15 +3,26 @@ import React from 'react';
 
 import {Cards, Charts, CountryPicker} from './components';
 import styles from './App.module.css';
+import { fetchData } from './api';
+
 
 class App extends React.Component {
+	state = {
+		data: {},
 
+	}
+
+	async componentDidMount() {
+		const fetcData = await fetchData();
+		this.setState({data: fetcData});
+	}
 	render() {
+		const {data} = this.state;
 		return (
 
 			<div className={styles.container}>
 
-			   <Cards />
+			   <Cards data={this.state.data}/>
 			   <CountryPicker />
 			   <Charts />
 
